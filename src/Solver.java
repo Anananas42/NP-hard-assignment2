552eeb4a8875d8a2f4c0b8463d8d270b7cf24bc1
Adam Pazderka <pazderka.adam@protonmail.com>
2024-03-17 22:16:25 +0100

    Add solutions for standard combinatorics

--- src/Solver.java
+++ src/Solver.java
@@ -1,54 +1,250 @@
 import java.util.*;
 
-class Solver {
-    static class Variable {
-        List<Integer> domain;
-        // you can add more attributes
+public class Solver {
+    // Workaround around the annoying template (it will probably also be on weblab...)
+    static int fixedPoint;
+    static Variable[] variables;
+    static Constraint[] constraints;
+    static List<int[]> solutions;
+
+    static class VariableAssignment {
+        public int fixedPointId;
+        public int value;
+
+        public VariableAssignment(int value, int fixedPointId) {
+            this.value = value;
+            this.fixedPointId = fixedPointId;
+        }
+
+        @Override
+        public String toString() {
+            return "<fixedPoint: " + this.fixedPointId + ", val: " + this.value + ">";
+        }
+    }
+
+    public static class Variable {
+        int id;
+        VariableAssignment assignment;
+        // Stack of propagations. Always one domain propagation per fixed point (may be same as previous domain).
+        Deque<List<Integer>> propagations;
+        // Constraints including this variable
+        int[] constraintIds;
+
+        final List<Integer> defaultDomain;
 
         /**
          * Constructs a new variable.
          * @param domain A list of values that the variable can take
          */
         public Variable(List<Integer> domain) {
-            this.domain = domain;
+            this.defaultDomain = new ArrayList<>(domain);
+        }
+
+        @Override
+        public String toString() {
+            if (assignment != null) {
+                return "[" + this.id + "]: " + assignment.toString();
+            }
+            return "[" + this.id + "]: " + propagations.peek().toString();
         }
     }
 
-    static abstract class Constraint {
+    // TODO: sort constraints by the number of unassigned variables (less = better)
+    public abstract static class Constraint {
+        int id;
+        Integer[] varIds;
+
         /**
          * Tries to reduce the domain of the variables associated to this constraint, using inference
+         * Also recursively infers other constraints if at least one reduction found
+         * 
+         * When inferring, current variable values are "assigned value" or if that is null, then the "propagations" domain on top of the stack
+         * 
+         * @return false if a reduction results in empty domain (= no solution possible)
          */
-        abstract void infer(/* you can add params */);
+        abstract boolean infer(int varId);
     }
 
-    // Example implementation of the Constraint interface.
-    // It enforces that for given variable X, it holds that 5 < X < 10.
-    //
-    // This particular constraint will most likely not be very useful to you...
-    // Remove it and design a few constraints that *can* help you!
-    static abstract class BetweenFiveAndTenConstraint {
-        Variable var;
+    public static class NeqConstraint extends Constraint {
 
-        public BetweenFiveAndTenConstraint(Variable var) {
-            this.var = var;
+        public NeqConstraint(int id1, int id2) {
+            this.varIds = new Integer[] { id1, id2 };
         }
 
-        void infer() {
-            List<Integer> newDomain = new LinkedList<>();
+        @Override
+        public boolean infer(int varId) {
+            int otherId;
+            if (this.varIds[1] == varId) {
+                otherId = this.varIds[0];
+            }else if (this.varIds[0] == varId) {
+                otherId = this.varIds[1];
+            }else {
+                throw new Error("[NeqConstraint] varId=" + varId + " not found in constraint varIds=" + Arrays.toString(this.varIds));
+            }
+            Variable currVar = variables[varId];
+            Variable otherVar = variables[otherId];
 
-            for (Integer x : this.var.domain) {
-                if (5 < x && x < 10)
-                    newDomain.add(x);
+            Integer valToRemove = currVar.assignment.value;
+            List<Integer> domain = otherVar.propagations.peek();
+
+            // If other variable assigned to the value that is being removed, no solution possible
+            if (otherVar.assignment != null) return otherVar.assignment.value != valToRemove;
+            
+            if (domain.contains(valToRemove)) {
+                domain.remove(valToRemove);
+
+                // Domain empty, which means no solution possible
+                if (domain.isEmpty()) return false;
+                
+                if (domain.size() != 1) return true;
+
+                // Domain restricted to size 1, which means the variable is now assigned to a value
+                otherVar.assignment = new VariableAssignment(domain.get(0), Solver.fixedPoint);
+
+                // Update new propagations based on assigned value
+                for (Integer cId : variables[otherId].constraintIds) {
+                    // If solution not possible as a result of propagation, return false
+                    if (!constraints[cId].infer(otherId)) return false;
+                }
             }
 
-            this.var.domain = newDomain;
+            return true;
         }
     }
 
-    Variable[] variables;
-    Constraint[] constraints;
-    List<int[]> solutions;
-    // you can add more attributes
+    public static class GrEqConstraint extends Constraint {
+        // id1 is greater or equal than id2
+        public GrEqConstraint(int id1, int id2) {
+            this.varIds = new Integer[] { id1, id2 };
+        }
+
+        @Override
+        public boolean infer(int varId) {
+            int otherId;
+            boolean otherIsLhs;
+            if (this.varIds[1] == varId) {
+                otherId = this.varIds[0];
+                otherIsLhs = true;
+            }else if (this.varIds[0] == varId) {
+                otherId = this.varIds[1];
+                otherIsLhs = false;
+            }else {
+                throw new Error("[GrEqConstraint] varId=" + varId + " not found in constraint varIds=" + Arrays.toString(this.varIds));
+            }
+
+            Variable currVar = variables[varId];
+            Variable otherVar = variables[otherId];
+
+            Integer currValue = currVar.assignment.value;
+            List<Integer> domain = otherVar.propagations.peek();
+
+            // Other variable assigned to the value that is being removed, no solution possible
+            if (otherVar.assignment != null) {
+                if (otherIsLhs) return otherVar.assignment.value >= currValue;
+                return currValue >= otherVar.assignment.value;
+            }
+
+            // Filter values to be removed
+            List<Integer> valsToRemove = new ArrayList<>();
+            for (Integer otherDomainVal : domain) {
+                if (otherIsLhs && otherDomainVal < currValue) valsToRemove.add(otherDomainVal);
+                if (!otherIsLhs && otherDomainVal > currValue) valsToRemove.add(otherDomainVal);
+            }
+
+            // No vals to remove, return
+            if (valsToRemove.isEmpty()) return true;
+
+            for (Integer toRemove : valsToRemove) {
+                domain.remove(toRemove);
+            }
+
+            // Domain empty, no solution possible
+            if (domain.size() == 0) return false;
+
+            if (domain.size() != 1) return true;
+
+            // Domain restricted to only one value, assign it
+            otherVar.assignment = new VariableAssignment(domain.get(0), fixedPoint);
+            
+            // Update new propagations
+            for (Integer cId : variables[otherId].constraintIds) {
+                // If solution not possible as a result of propagation, return false
+                if (!constraints[cId].infer(otherId)) return false;
+            }
+
+            return true;
+        }
+    }
+
+    public static class GrConstraint extends Constraint {
+        // id1 is greater or equal than id2
+        public GrConstraint(int id1, int id2) {
+            this.varIds = new Integer[] { id1, id2 };
+        }
+
+        @Override
+        public boolean infer(int varId) {
+            int otherId;
+            boolean otherIsLhs;
+            if (this.varIds[1] == varId) {
+                otherId = this.varIds[0];
+                otherIsLhs = true;
+            }else if (this.varIds[0] == varId) {
+                otherId = this.varIds[1];
+                otherIsLhs = false;
+            }else {
+                throw new Error("[GrConstraint] varId=" + varId + " not found in constraint varIds=" + Arrays.toString(this.varIds));
+            }
+
+            Variable currVar = variables[varId];
+            Variable otherVar = variables[otherId];
+
+            Integer currValue = currVar.assignment.value;
+            List<Integer> domain = otherVar.propagations.peek();
+
+            // Other variable assigned to the value that is being removed, no solution possible
+            if (otherVar.assignment != null) {
+                if (otherIsLhs) return otherVar.assignment.value > currValue;
+                return currValue > otherVar.assignment.value;
+            }
+
+            // Filter values to be removed
+            List<Integer> valsToRemove = new ArrayList<>();
+            for (Integer otherDomainVal : domain) {
+                if (otherIsLhs && otherDomainVal <= currValue) valsToRemove.add(otherDomainVal);
+                if (!otherIsLhs && otherDomainVal >= currValue) valsToRemove.add(otherDomainVal);
+            }
+
+            // No vals to remove, return
+            if (valsToRemove.isEmpty()) return true;
+
+            for (Integer toRemove : valsToRemove) {
+                domain.remove(toRemove);
+            }
+
+            // Domain empty, no solution possible
+            if (domain.size() == 0) return false;
+
+            if (domain.size() != 1) return true;
+
+            // Domain restricted to only one value, assign it
+            otherVar.assignment = new VariableAssignment(domain.get(0), fixedPoint);
+            
+            // Update new propagations
+            for (Integer cId : variables[otherId].constraintIds) {
+                // If solution not possible as a result of propagation, return false
+                if (!constraints[cId].infer(otherId)) return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "<GrConstraint:  x" + this.varIds[0] + " > x" + this.varIds[1] + "  >";
+        }
+    }
+
 
     /**
      * Constructs a solver.
@@ -56,10 +252,40 @@ class Solver {
      * @param constraints The constraints applied to the variables
      */
     public Solver(Variable[] variables, Constraint[] constraints) {
-        this.variables = variables;
-        this.constraints = constraints;
+        Solver.fixedPoint = 0;
+        Solver.variables = new Variable[variables.length];
+        for (int i = 0; i < variables.length; i++) {
+            Solver.variables[i] = variables[i];
+            Solver.variables[i].id = i;
+            Solver.variables[i].assignment = null;
+            Solver.variables[i].propagations = new ArrayDeque<>();
+            Solver.variables[i].propagations.push(new ArrayList<>(Solver.variables[i].defaultDomain));
+        }
 
-        solutions = new LinkedList<>();
+        Solver.constraints = new Constraint[constraints.length];
+        for (int i = 0; i < constraints.length; i++) {
+            Solver.constraints[i] = constraints[i];
+            Solver.constraints[i].id = i;
+        }
+
+        for (Variable var : Solver.variables) {
+            List<Constraint> varConstraints = new ArrayList<>();
+            for (Constraint constr : Solver.constraints) {
+                // Add constraint to variable's list if the constraint affects the variable
+                for (Integer varId : constr.varIds) {
+                    if (varId == var.id) {
+                        varConstraints.add(constr);
+                        break;
+                    }
+                }
+            }
+            var.constraintIds = new int[varConstraints.size()];
+            for (int i = 0; i < varConstraints.size(); i++) {
+                var.constraintIds[i] = varConstraints.get(i).id;
+            }
+        }
+
+        Solver.solutions = new LinkedList<>();
     }
 
     /**
@@ -96,6 +322,64 @@ class Solver {
      * Solves the problem using search and inference.
      */
     void search(boolean findAllSolutions /* you can add more params */) {
-        // TODO: implement search using search and inference
+        
+        // if not findAllSolutions and solutions not empty, return
+        if (!findAllSolutions && !Solver.solutions.isEmpty()) return;
+
+        // Find unassigned variable
+        Variable unassignedVar = null;
+        for (Variable var : Solver.variables) {
+            if (var.assignment != null) continue;
+            unassignedVar = var;
+            break;
+        }
+
+        // If all variables assigned, solution found
+        if (unassignedVar == null) {
+            int[] solution = new int[Solver.variables.length];
+            for (int i = 0; i < Solver.variables.length; i++) {
+                solution[i] = Solver.variables[i].assignment.value;
+            }   
+            Solver.solutions.add(solution);
+            return;
+        }
+
+        // Try all possible assignments
+        for (Integer val : unassignedVar.propagations.peek()) {
+            Solver.fixedPoint++;
+
+            int currFixedPoint = Solver.fixedPoint;
+            boolean isSolutionStillPossible = true;
+
+            // Add new propagation domains (copy last)
+            for (Variable variable : Solver.variables) {
+                variable.propagations.push(new ArrayList<>(variable.propagations.peek()));
+            }
+
+            // Perform an assignment and propagation pass 
+                // if propagation restricts domain to empty list, no solution possible
+                // if after propagation domain has only one number, create assignment
+            unassignedVar.assignment = new VariableAssignment(val, currFixedPoint);
+            for (Integer cId : unassignedVar.constraintIds) {
+                if (Solver.constraints[cId].infer(unassignedVar.id)) continue;
+                isSolutionStillPossible = false;
+                break;
+            }
+            
+            // if solution is possible
+                // Call this method recursively with updated assignment and propagations
+            
+            if (isSolutionStillPossible) search(findAllSolutions);
+
+            // Pop assignment and propagation
+            for (Variable popVariable : Solver.variables) {
+                if (popVariable.assignment != null && popVariable.assignment.fixedPointId == currFixedPoint) {
+                    popVariable.assignment = null;
+                }
+                popVariable.propagations.pop();
+            }
+
+        }
+
     }
 }
--- src/StandardCombinatorics.java
+++ src/StandardCombinatorics.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StandardCombinatorics {
     /**
@@ -10,6 +11,9 @@ public class StandardCombinatorics {
         List<Solver.Constraint> constraints = new ArrayList<>();
 
         // TODO: add your variables
+        for (int i = 0; i < n; i++) {
+            variables.add(new Solver.Variable(List.of(0, 1)));
+        }
 
         // TODO: add your constraints
 
@@ -24,7 +28,12 @@ public class StandardCombinatorics {
         List<int[]> result = solver.findAllSolutions();
 
         // TODO: use result to construct answer
-        return new ArrayList<>();
+        List<String> resultStrings = result.stream()
+                                        .map(arr -> Arrays.stream(arr)
+                                            .mapToObj(String::valueOf)
+                                            .collect(Collectors.joining()))
+                                        .collect(Collectors.toList());
+        return resultStrings;
     }
 
     /**
@@ -35,9 +44,18 @@ public class StandardCombinatorics {
         List<Solver.Variable> variables = new ArrayList<>();
         List<Solver.Constraint> constraints = new ArrayList<>();
 
-        // TODO: add your variables
+        List<Integer> domain = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            domain.add(i+1);
+        }
 
-        // TODO: add your constraints
+        for (int i = 0; i < k; i++){
+            variables.add(new Solver.Variable(domain));
+        }
+
+        for (int i = 0; i < k-1; i++) {
+            constraints.add(new Solver.GrConstraint(i+1, i));
+        }
 
         // Convert to arrays
         Solver.Variable[] variablesArray = new Solver.Variable[variables.size()];
@@ -49,8 +67,12 @@ public class StandardCombinatorics {
         Solver solver = new Solver(variablesArray, constraintsArray);
         List<int[]> result = solver.findAllSolutions();
 
-        // TODO: use result to construct answer
-        return new ArrayList<>();
+        // System.out.println("Solutions:");
+        // for (int[] solution : result) {
+        //     System.out.println(Arrays.toString(solution));
+        // }
+
+        return result;
     }
 
     /**
@@ -61,9 +83,18 @@ public class StandardCombinatorics {
         List<Solver.Variable> variables = new ArrayList<>();
         List<Solver.Constraint> constraints = new ArrayList<>();
 
-        // TODO: add your variables
+        List<Integer> domain = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            domain.add(i+1);
+        }
 
-        // TODO: add your constraints
+        for (int i = 0; i < k; i++){
+            variables.add(new Solver.Variable(domain));
+        }
+
+        for (int i = 0; i < k-1; i++) {
+            constraints.add(new Solver.GrEqConstraint(i+1, i));
+        }
 
         // Convert to arrays
         Solver.Variable[] variablesArray = new Solver.Variable[variables.size()];
@@ -75,34 +106,23 @@ public class StandardCombinatorics {
         Solver solver = new Solver(variablesArray, constraintsArray);
         List<int[]> result = solver.findAllSolutions();
 
-        // TODO: use result to construct answer
-        return new ArrayList<>();
+        return result;
     }
 
     /**
      * Returns a list of all subsets in the set {1,...,n}
      */
     public static List<int[]> getSubsets(int n) {
-        // Initialize lists for variables and constraints
-        List<Solver.Variable> variables = new ArrayList<>();
-        List<Solver.Constraint> constraints = new ArrayList<>();
 
-        // TODO: add your variables
+        List<int[]> result = new ArrayList<>();
+        int[] emptySet = new int[0];
+        result.add(emptySet);
 
-        // TODO: add your constraints
+        for (int k = 0; k < n; k++) {
+            result.addAll(getCombinationsWithoutRepetition(n, k));
+        }
 
-        // Convert to arrays
-        Solver.Variable[] variablesArray = new Solver.Variable[variables.size()];
-        variablesArray = variables.toArray(variablesArray);
-        Solver.Constraint[] constraintsArray = new Solver.Constraint[constraints.size()];
-        constraintsArray = constraints.toArray(constraintsArray);
-
-        // Use solver
-        Solver solver = new Solver(variablesArray, constraintsArray);
-        List<int[]> result = solver.findAllSolutions();
-
-        // TODO: use result to construct answer
-        return new ArrayList<>();
+        return result;
     }
 
     /**
@@ -113,9 +133,20 @@ public class StandardCombinatorics {
         List<Solver.Variable> variables = new ArrayList<>();
         List<Solver.Constraint> constraints = new ArrayList<>();
 
-        // TODO: add your variables
+        List<Integer> domain = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            domain.add(i+1);
+        }
 
-        // TODO: add your constraints
+        for (int i = 0; i < n; i++){
+            variables.add(new Solver.Variable(domain));
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                constraints.add(new Solver.NeqConstraint(i, j));
+            }
+        }
 
         // Convert to arrays
         Solver.Variable[] variablesArray = new Solver.Variable[variables.size()];
@@ -127,7 +158,11 @@ public class StandardCombinatorics {
         Solver solver = new Solver(variablesArray, constraintsArray);
         List<int[]> result = solver.findAllSolutions();
 
-        // TODO: use result to construct answer
-        return new ArrayList<>();
+        // System.out.println("Solutions:");
+        // for (int[] solution : result) {
+        //     System.out.println(Arrays.toString(solution));
+        // }
+
+        return result;
     }
 }
